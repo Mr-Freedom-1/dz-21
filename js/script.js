c19a1793dc9d7b2854ad81ec0da3c368e8553e71
Mr-Freedom-1 <glebpoterailo@gmail.com>
2023-01-03 15:21:46 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,82 +1,82 @@
 /*-------------Завдання №1-------------*/
-// const car = {
-//     model: 'BMW',
-//     year: 2022,
-//     color: 'black',
-// }
+const car = {
+    model: 'BMW',
+    year: 2022,
+    color: 'black',
+}
 
-// car.color = 'red';
+car.color = 'red';
 
-// car.type = 'electric';
+car.type = 'electric';
 
-// car.signal = function(){
-//     alert('fa! fa!');
-// } 
+car.signal = function(){
+    alert('fa! fa!');
+} 
 
-// console.log(car);
-// car.signal();
+console.log(car);
+car.signal();
 
 /*-------------Завдання №2-------------*/
-// let salaries = {
-//     frontend: 12000,
-//     backend: 10000,
-//     designer: 8000,
-//     dayPay() {
-//         alert('We must pay salary on Tuesday!');
-//     },
-//     total() {
-//         let sum = 0;
-//         for (let key in this){
-//             if (typeof this[key] === 'number'){
-//                 sum += this[key];
-//             }
-//         }
-//         console.log(sum);
-//     },
-// }
+let salaries = {
+    frontend: 12000,
+    backend: 10000,
+    designer: 8000,
+    dayPay() {
+        alert('We must pay salary on Tuesday!');
+    },
+    total() {
+        let sum = 0;
+        for (let key in this){
+            if (typeof this[key] === 'number'){
+                sum += this[key];
+            }
+        }
+        console.log(sum);
+    },
+}
 
-// console.log(salaries);
-// salaries.total();
-// salaries.manager = 5000;
-// console.log(salaries);
-// salaries.total();
+console.log(salaries);
+salaries.total();
+salaries.manager = 5000;
+console.log(salaries);
+salaries.total();
 
 /*-------------Завдання №3-------------*/
-// function Firm(brand, system, cost){
-//     this.brand = brand;
-//     this.system = system;
-//     this.cost = cost;
-// }
+function Firm(brand, system, cost){
+    this.brand = brand;
+    this.system = system;
+    this.cost = cost;
+}
 
-// const dell = new Firm('Dell', 'windows', 800);
-// const apple = new Firm('Apple', 'MAC OS', 1700);
+const dell1 = new Firm('Dell', 'windows', 800);
+const apple1 = new Firm('Apple', 'MAC OS', 1700);
 
-// console.log(dell);
-// console.log(apple);
+console.log(dell1);
+console.log(apple1);
 
 /*-------------Завдання №4-------------*/
-// function Firm(brand, system, cost){
-//         this.brand = brand;
-//         this.system = system;
-//         this.cost = cost;
-//         this[Symbol.toPrimitive] = function (hint){
-//             console.log(hint);
-//             switch(hint){
-//                 case 'string':
-//                     return this.brand
-//                 case 'string':
-//                     return this.system
-//                 case 'number':
-//                     return this.cost
-//                 case 'default':
-//                     return this.brand + ' ' + this.system + ' ' + this.cost + ' // '
-//             }
-//         }
-//     }
+function Firm(brand, system, cost){
+        this.brand = brand;
+        this.system = system;
+        this.cost = cost;
+        this[Symbol.toPrimitive] = function (hint){
+            console.log(hint);
+            switch(hint){
+                case 'string':
+                    return this.brand
+                case 'string':
+                    return this.system
+                case 'number':
+                    return this.cost
+                case 'default':
+                    return this.brand + ' ' + this.system + ' ' + this.cost + ' // '
+            }
+        }
+    }
     
-// const dell = new Firm('Dell', 'windows', 800);
-// const apple = new Firm('Apple', 'MAC OS', 1700);
+const dell = new Firm('Dell', 'windows', 800);
+const apple = new Firm('Apple', 'MAC OS', 1700);
 
-// console.log(String(dell));
-// console.log(+apple);
-// console.log(dell + apple);
+console.log(String(dell));
+console.log(+apple);
+console.log(dell + apple);
